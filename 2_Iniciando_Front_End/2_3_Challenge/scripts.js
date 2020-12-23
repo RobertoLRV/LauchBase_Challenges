d2584b942f8b836b7781058ca07114ea278ee556
Roberto Luiz <luizroberto1957@hotmail.com>
2020-12-22 22:25:19 -0300

    chore: Implementation Maximize/Minimize and Close

---
+++ 2_Iniciando_Front_End/2_3_Challenge/scripts.js
@@ -0,0 +1,29 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+
+const cards = document.querySelectorAll('.card');
+
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const postId = card.getAttribute("id")
+        modalOverlay.classList.add('active')
+
+        modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${postId}`
+    })
+}
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+ 
+    if(modal.className == "modal maximize" ){
+        modal.classList.remove("maximize");
+    }else{
+        modal.classList.add("maximize");
+    }
+})
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = ""
+    modal.classList.remove("maximize");
+})
+
